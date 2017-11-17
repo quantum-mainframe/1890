561053a77e73852dcb0e464ad41b7fa21e24e4af
whizzer0 <whizzerZero@gmail.com>
2017-11-17 18:27:11 +0000

    Fixes and can be properly played now

--- demo.py
+++ demo.py
@@ -1,25 +1,36 @@
-import configparser
+import configparser, os
 from fight import fightOutcome
 
 objects = configparser.ConfigParser()
 objects.read('objects.ini')
 
-p1 = 'gun'
-p2 = 'knife'
+p1 = input("Enter the id of the object player 1 wants to use: ")
+p1s = input("Does player 1 want to play offensively or defensively? ")
+if p1s[0] == 'o':
+    p1s = 'offense'
+else:
+    p1s = 'defense'
+os.system('clear')
+p2 = input("Enter the id of the object player 2 wants to use: ")
+p2s = input("Does player 2 want to play offensively or defensively? ")
+if p2s[0] == 'o':
+    p2s = 'offense'
+else:
+    p2s = 'defense'
 
-def runFight(p1, p2):
+def runFight(p1, p2, p1s, p2s):
     o = objects[p1]
-    obj1 = {'name':o['Name'], 'atk':int(o['Atk']), 'def':int(o['Def']), 'style':int(o['Style']), 'textLose':o['TextLose'], 'textWin':o['TextWin'], 'state':'offense'}
+    obj1 = {'name':o['Name'], 'atk':int(o['Atk']), 'def':int(o['Def']), 'style':int(o['Style']), 'textLose':o['TextLose'], 'textWin':o['TextWin'], 'state':p1s}
     o = objects[p2]
-    obj2 = {'name':o['Name'], 'atk':int(o['Atk']), 'def':int(o['Def']), 'style':int(o['Style']), 'textLose':o['TextLose'], 'textWin':o['TextWin'], 'state':'defense'}
+    obj2 = {'name':o['Name'], 'atk':int(o['Atk']), 'def':int(o['Def']), 'style':int(o['Style']), 'textLose':o['TextLose'], 'textWin':o['TextWin'], 'state':p2s}
     
     if obj1['state'] == 'offense' and obj2['state'] == 'defense':
         print("The {0} attacks the {1}!".format(obj1['name'], obj2['name']))
-    elif obj1[state] == 'defense' and obj2[state] == 'offense':
+    elif obj1['state'] == 'defense' and obj2['state'] == 'offense':
         print("The {0} is attacked by the {1}!".format(obj1['name'], obj2['name']))
-    elif obj1[state] == 'offense' and obj2[state] == 'offense':
+    elif obj1['state'] == 'offense' and obj2['state'] == 'offense':
         print("The {0} and {1} fight!".format(obj1['name'], obj2['name']))
-    elif obj1[state] == 'defence' and obj2[state] == 'defence':
+    elif obj1['state'] == 'defence' and obj2['state'] == 'defence':
         print("The {0} and {1} defend the hell out of each other!".format(obj1['name'], obj2['name']))
     winner = fightOutcome(obj1, obj2)
     print("The {} wins!".format(winner['name']))
@@ -29,4 +40,4 @@ def runFight(p1, p2):
     elif winner == obj2:
         print(obj1['textLose'].format(obj2['textWin']))
 
-runFight(p1, p2)
+runFight(p1, p2, p1s, p2s)
--- fight.py
+++ fight.py
@@ -9,7 +9,7 @@ def fightOutcome(obj1, obj2):
             return obj1
         else:
             return obj2
-    elif obj1[state] == 'defense' and obj2[state] == 'offense':
+    elif obj1['state'] == 'defense' and obj2['state'] == 'offense':
         if obj2['atk'] == obj1['def']:
             if obj2['style'] > obj1['style']:
                 return obj2
@@ -19,7 +19,7 @@ def fightOutcome(obj1, obj2):
             return obj2
         else:
             return obj1
-    elif obj1[state] == 'offense' and obj2[state] == 'offense':
+    elif obj1['state'] == 'offense' and obj2['state'] == 'offense':
         if obj1['atk'] == obj2['atk']:
             if obj1['style'] > obj2['style']:
                 return obj1
@@ -29,7 +29,7 @@ def fightOutcome(obj1, obj2):
             return obj1
         else:
             return obj2
-    elif obj1[state] == 'defence' and obj2[state] == 'defence':
+    elif obj1['state'] == 'defense' and obj2['state'] == 'defense':
         if obj1['def'] == obj2['def']:
             if obj1['style'] > obj2['style']:
                 return obj1
